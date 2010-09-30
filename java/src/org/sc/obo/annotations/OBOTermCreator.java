778f48179802cf0ab15c8f8d189f81c2550e6811
Timothy Danford <tdanford@gmail.com>
2010-09-30 17:24:28 -0400

    Added @Term creation from OBOTerm objects.
    Updated OBOTermCreator to create @Term-annotated Class objects from
    OBOTerm objects derived from an OBOOntology.  The trick here is that we
    have to *recursively* create terms for the super-classes as well, and
    (finally) we don't want to repeatedly do that -- so we cache the previously
    created terms by ID, which should be unique.

--- java/src/org/sc/obo/annotations/OBOTermCreator.java
+++ java/src/org/sc/obo/annotations/OBOTermCreator.java
@@ -1,5 +1,16 @@
 package org.sc.obo.annotations;
 
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.sc.obo.OBOOntology;
+import org.sc.obo.OBOStanza;
+import org.sc.obo.OBOTerm;
+import org.sc.obo.OBOValue;
+
 import javassist.*;
 import javassist.bytecode.AnnotationsAttribute;
 import javassist.bytecode.ClassFile;
@@ -10,10 +21,11 @@ public class OBOTermCreator {
 	
 	public static void main(String[] args) { 
 		try {
-			Class cls = createTerm("request_0010",
+			OBOTermCreator creator = new OBOTermCreator();
+			Class cls = creator.createTerm("request_0010",
 					"foo bar", 
 					"a test term", null, 
-					new Class[] { ProteinSite.class });
+					new Class[] { ProteinSite.class }, null, null);
 			
 			OBOAnnotationParser obo = new OBOAnnotationParser();
 			System.out.println(obo.stanza(cls));
@@ -24,13 +36,63 @@ public class OBOTermCreator {
 			e.printStackTrace(System.err);
 		}
 	}
+	
+	private Map<String,Class> created;
+	
+	public OBOTermCreator() {
+		created = new TreeMap<String,Class>();
+	}
+	
+	public Class createTerm(OBOOntology ontology, OBOTerm termStanza) throws CannotCompileException {
 
-	public static Class createTerm(
+		String id = termStanza.id();
+		if(created.containsKey(id)) { 
+			return created.get(id);
+		}
+
+		String name = termStanza.getName();
+		String def = termStanza.values("def").get(0).getValue();
+		
+		List<OBOValue> isaValues = termStanza.values("is_a");
+		Class[] is_a = new Class[isaValues.size()];
+		for(int i = 0; i < isaValues.size(); i++) { 
+			OBOValue value = isaValues.get(i);
+			String valueStr = value.getValue();
+			String isaID = valueStr.split("!")[0].trim();
+			OBOTerm isaTerm = (OBOTerm)ontology.getStanza(isaID);
+			is_a[i] = createTerm(ontology, isaTerm);
+		}
+		
+		Pattern relPattern = Pattern.compile("^\\s*([^\\s]+)\\s+(.*)\\s*!\\s*(.*)$");
+		
+		List<OBOValue> relValues = termStanza.values("relationship");
+		Class[] relTypes = new Class[relValues.size()];
+		String[] relTypedefs = new String[relValues.size()];
+		for(int i = 0; i < relValues.size(); i++) { 
+			OBOValue relValue = relValues.get(i);
+			Matcher relMatcher = relPattern.matcher(relValue.getValue());
+			if(relMatcher.matches()) { 
+				OBOTerm relTerm = (OBOTerm)ontology.getStanza(relMatcher.group(2));
+				relTypes[i] = createTerm(ontology, relTerm);
+				relTypedefs[i] = relMatcher.group(1);
+			}
+		}
+
+		return createTerm(id, name, def, null, is_a, relTypes, relTypedefs);
+	}
+
+	public Class createTerm(
 			String id, 
 			String name, 
 			String def, 
 			String[] comments, 
-			Class[] is_a) throws CannotCompileException { 
+			Class[] is_a, 
+			Class[] relTypes, 
+			String[] relTypedefs) throws CannotCompileException {
+		
+		if(created.containsKey(id)) { 
+			return created.get(id);
+		}
 
 		OBOAnnotationParser obo = new OBOAnnotationParser();
 		
@@ -46,7 +108,14 @@ public class OBOTermCreator {
 		
         CtClass cc = cp.makeInterface(OntologyAnnotationParser.camelCase(name, "\\s+"));
         cc.setModifiers(javassist.Modifier.INTERFACE | javassist.Modifier.PUBLIC);
-        
+
+        ClassFile ccFile = cc.getClassFile();
+        ConstPool constpool = ccFile.getConstPool();
+
+        Annotation termAnnotation = new Annotation("org.sc.obo.annotations.Term", constpool);
+        Annotation relAnnotation = new Annotation("org.sc.obo.annotations.Relates", constpool);
+        //annot.addMemberValue("value", new IntegerMemberValue(ccFile.getConstPool(), 0));
+
         CtField idField = new CtField(stringClass, "id", cc);
         idField.setModifiers(javassist.Modifier.PUBLIC | javassist.Modifier.STATIC | javassist.Modifier.FINAL);
 
@@ -74,19 +143,34 @@ public class OBOTermCreator {
         		}
         	}
         }
-
-        ClassFile ccFile = cc.getClassFile();
-        ConstPool constpool = ccFile.getConstPool();
+        
+        if(relTypes != null && relTypedefs != null) { 
+        	if(relTypes.length != relTypedefs.length) { throw new IllegalArgumentException(); }
+        	for(int i = 0; i < relTypes.length; i++) { 
+        		try {
+					CtClass relTypeClass = cp.get(relTypes[i].getCanonicalName());
+					CtMethod relMethod = new CtMethod(relTypeClass, relTypedefs[i], new CtClass[]{}, cc);
+					relMethod.setModifiers(Modifier.PUBLIC);
+					
+					AnnotationsAttribute annotations = 
+						(AnnotationsAttribute) relMethod.getMethodInfo().getAttribute(AnnotationsAttribute.visibleTag);
+					annotations.addAnnotation(relAnnotation);
+			        
+			        cc.addMethod(relMethod);
+			        
+				} catch (NotFoundException e) {
+        			throw new IllegalArgumentException(e);
+				}
+        	}
+        }
 
         AnnotationsAttribute attr = new AnnotationsAttribute(constpool, AnnotationsAttribute.visibleTag);
-        Annotation annot = new Annotation("org.sc.obo.annotations.Term", constpool);
-
-        //annot.addMemberValue("value", new IntegerMemberValue(ccFile.getConstPool(), 0));
-        
-        attr.addAnnotation(annot);
+        attr.addAnnotation(termAnnotation);
         ccFile.addAttribute(attr);
         
         Class c = cc.toClass();
+        created.put(id, c);
+        
 		return c;
 	}
 }
